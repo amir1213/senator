8612bec5c2db73b3e780b9af99af5b3f8895d255
hafez16 <janboofi.uoovh@gmail.com>
2016-01-28 14:16:10 +0330

    Update block.lua

--- plugins/block.lua
+++ plugins/block.lua
@@ -1,36 +1,27 @@
 do
-local function block_user_callback(cb_extra, success, result)
-  local receiver = cb_extra.receiver
-  local user = 'user#id'..result.id
-  if success == 0 then
-    return send_large_msg(receiver, "Block Process Failedًں”´آ´")
-  end
-  block_user(user, cb_ok, false)
-end
+	local function block_user_callback(cb_extra, success, result)
+		local user = 'user#id'..result.id
+		if success == 0 then
+			return "اين يوزر بلوک نميشود"
+		end
+		block_user(user, cb_ok, false)
+	end
 end
 
 local function run(msg, matches)
- if msg.to.type == 'chat' then
-    local user = 'chat#id'..msg.to.id
- local user = matches[2]
-  if matches[1] == "user" then
-      user = 'user#id'..user
-      block_user(user, callback, false)
-    end
-	        if not is_sudo(msg) or is_realm(msg) and is_admin(msg) then
-    return 'Sudo Zone!'
-  end
-    return "I Have Blocked Userâœ…"
-  end
+	local user = 'user#id'..matches[1]
+	block_user(user, callback, false)
+    return "انجام شد"
 end
 
 return {
-  patterns = {
-    "^[!/]block (user) (%d+)$",
-  },
-  run = run,
+	description = "User Blocker", 
+	usage = {
+		"block (id) : بلاک افراد",
+	},
+	patterns = {
+		"^[Bb]lock (%d+)$",
+	}, 
+	run = run,
+	privileged = true
 }
---I'm Not Sure WoRks Or NOT!
---IT HAS A BUG THAT EVERYONE CAN BLOCK USERS [EVERYONE]
-    --https://github.com/ThisIsArman
-    --Telegram.me/ThisIsArman
